02e483e2ba869385781a8daf2382367536aa1a03
Martin Ueding <dev@martin-ueding.de>
2012-07-09 19:25:16 +0200

    Lowercase variables

--- TYPO3-Documentation-clone.sh
+++ TYPO3-Documentation-clone.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Enter your username for typo3.org here
-USERNAME=""
+username=""
 dryrun=true
 
 if [[ -z "$USERNAME" ]]; then
-	read -p "Please enter your typo3.org username: " -r USERNAME
+	read -p "Please enter your typo3.org username: " -r username
 fi
 
 # Fetch the list of projects
-list_of_projects="$(ssh "$USERNAME@review.typo3.org" -p 29418 "gerrit ls-projects" | grep -e "^Documentation/TYPO3")"
+list_of_projects="$(ssh "$username@review.typo3.org" -p 29418 "gerrit ls-projects" | grep -e "^Documentation/TYPO3")"
 
 if [[ "$dryrun" = "true" ]]
 then
