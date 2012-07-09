255af40ab4aca5482fa34f41f11073aa1446c25a
Martin Ueding <dev@martin-ueding.de>
2012-07-09 19:14:03 +0200

    Put sed command into each line

--- TYPO3-Documentation-clone.sh
+++ TYPO3-Documentation-clone.sh
@@ -8,12 +8,12 @@ if [[ -z "$USERNAME" ]]; then
 fi
 
 # Fetch the list of projects
-list_of_projects="$(ssh "$USERNAME@review.typo3.org" -p 29418 "gerrit ls-projects" | grep -e "^Documentation/TYPO3" | sed 's/ /\n/g')"
+list_of_projects="$(ssh "$USERNAME@review.typo3.org" -p 29418 "gerrit ls-projects" | grep -e "^Documentation/TYPO3")"
 
 # Dry run mode, will only output the commands
-echo $list_of_projects | parallel --jobs 1 'echo "mkdir -p $(pwd)/{1}"'
-echo $list_of_projects | parallel --jobs 2 'echo "cd $(pwd)/{1}; echo {1}; git clone --recursive git://git.typo3.org/{1}.git ."'
+echo $list_of_projects | sed 's/ /\n/g' | parallel --jobs 1 'echo "mkdir -p $(pwd)/{1}"'
+echo $list_of_projects | sed 's/ /\n/g' | parallel --jobs 2 'echo "cd $(pwd)/{1}; echo {1}; git clone --recursive git://git.typo3.org/{1}.git ."'
 
 # Execute commands
-echo $list_of_projects | parallel --jobs 1 "mkdir -p $(pwd)/{1}"
-echo $list_of_projects | parallel --jobs 2 "cd $(pwd)/{1}; echo {1}; git clone --recursive git://git.typo3.org/{1}.git ."
+echo $list_of_projects | sed 's/ /\n/g' | parallel --jobs 1 "mkdir -p $(pwd)/{1}"
+echo $list_of_projects | sed 's/ /\n/g' | parallel --jobs 2 "cd $(pwd)/{1}; echo {1}; git clone --recursive git://git.typo3.org/{1}.git ."
