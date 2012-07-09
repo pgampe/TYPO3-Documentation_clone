fc54d4591dbf0e4855d224f7e2f6451f3e508a7f
Philipp Gampe <dev@philippgampe.info>
2012-07-09 20:22:58 +0200

    Remove dry run

--- TYPO3-Documentation-clone.sh
+++ TYPO3-Documentation-clone.sh
@@ -12,57 +12,33 @@ if [[ -z "$username" ]]; then
 	read -p "Please enter your typo3.org username: " -r username
 fi
 
-# Asks the user if he wants to perform a dry run only.
-read -p "Do you want to actually run the commands? [y/N] " answer
-
-if [[ "$answer" = [yYjJ] ]]
-then
-	dryrun=false
-	echo "Performing the actual commands."
-else
-	dryrun=true
-	echo "Performing a dry run only."
-fi
-
 # Fetch the list of projects
 list_of_projects="$(ssh "$username@review.typo3.org" -p 29418 "gerrit ls-projects" | grep -e "^Documentation/TYPO3")"
 
 start_dir="$(pwd)"
 
-if [[ "$dryrun" = "true" ]]
-then
-	for project in $list_of_projects
-	do
-		echo "$project"
-		project_dir="$(pwd)/$project"
-		echo mkdir -p "$project_dir"
-		echo cd "$project_dir"
-		echo git clone --recursive "git://git.typo3.org/$project.git" .
-	done
-else
-	for project in $list_of_projects
-	do
-		echo "$project"
+for project in $list_of_projects
+do
+	echo "$project"
 
-		# Create a new directory for the git repository.
-		project_dir="$(pwd)/$project"
-		mkdir -p "$project_dir"
-		cd "$project_dir"
+	# Create a new directory for the git repository.
+	project_dir="$(pwd)/$project"
+	mkdir -p "$project_dir"
+	cd "$project_dir"
 
-		# Clone the actual repository.
-		git clone --recursive "git://git.typo3.org/$project.git" .
+	# Clone the actual repository.
+	git clone --recursive "git://git.typo3.org/$project.git" .
 
-		# Fetch gerrit commit hook
-		scp -p -P 29418 "$username@review.typo3.org:hooks/commit-msg" .git/hooks/
+	# Fetch gerrit commit hook
+	scp -p -P 29418 "$username@review.typo3.org:hooks/commit-msg" .git/hooks/
 
-		# Init potential submodules
-		if [[ -n "$(git submodule status)" ]]
-		then
-			git submodule update --init
-			git submodule foreach "scp -p -P 29418 \"$username@review.typo3.org:hooks/commit-msg\" .git/hooks/"
-		fi
+	# Init potential submodules
+	if [[ -n "$(git submodule status)" ]]
+	then
+		git submodule update --init
+		git submodule foreach "scp -p -P 29418 \"$username@review.typo3.org:hooks/commit-msg\" .git/hooks/"
+	fi
 
-		# Return to the old directory.
-		cd "$start_dir"
-	done
-fi
+	# Return to the old directory.
+	cd "$start_dir"
+done
